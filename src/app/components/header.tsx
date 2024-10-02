958f828522c1997097e7f83dc55fa41545153e9d
Azharkhan3310 <muhammadzahar258@gmail.com>
2024-10-02 22:57:10 +0500

    Update header.tsx

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,7 +3,7 @@ export function Header() {
         <div className="header">
             <ul className="buttons">
                 <li><a href="http://localhost:3000/"> Home </a></li>
-                <li><a href="http://localhost:3000/portfolio"> Portfolio </a></li>
+                <li><a href="/portfolio"> Portfolio </a></li>
                 <li><a href="/about"> About </a></li>
                 <li><a href="/contact-us"> Contact Us </a></li>
             </ul>
